95b54a92e5844906f6bf20360d4d1befc42aec11
endearingelephant978 <114711975+endearingelephant978@users.noreply.github.com>
2022-11-15 00:29:16 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,28 +4,61 @@ import bakeryData from "./assets/bakery-data.json";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
-  item.image = process.env.PUBLIC_URL + "/" + item.image;
+    item.image = process.env.PUBLIC_URL + "/" + item.image;
 });
 /* ############################################################## */
 
 function App() {
-  // TODO: use useState to create a state variable to hold the state of the cart
-  /* add your cart state code here */
+    // TODO: use useState to create a state variable to hold the state of the cart
+    /* add your cart state code here */
+    const [cart, setCart] = useState([])
+    
+    const addToCart = (index) => {
+        setCart([...cart, index])
+    }
 
-  return (
-    <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+    const prices = cart.map((index) => (bakeryData[index].price))
+    let total = prices.reduce((a,b) => a + b, 0)
+    total = Math.round((total + Number.EPSILON) * 100) / 100
 
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
+    return (
+        <div className="App">
+            <h1>🧁 The Bakery 🧁</h1>
+            {bakeryData.map((item, index) => (
+                <BakeryItem name={item.name} desc={item.description} price={item.price} image={item.image} onAddToCart={() => addToCart(index)} />
+            ))}
 
-      <div>
-        <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
-      </div>
-    </div>
-  );
+            <div>
+                <h2>🛒 Cart 🛒</h2>
+                {cart.map((index) => (
+                    <CartItem index={index}/>
+                ))}
+            </div>
+
+            <h1>Your total is ${total} 💰</h1> 
+        </div>
+    );
+}
+
+function BakeryItem(props) {
+    return (
+        <>
+            <h3>{props.name}</h3>
+            <p>{props.desc}</p>
+            <div>
+                <img src={props.image} alt={'picture of ' + props.name}></img>
+            </div>
+            <button onClick={props.onAddToCart}>add to cart for ${props.price}</button>
+        </>
+    )
+}
+
+function CartItem(props) {
+    return (
+        <>
+            <p>{bakeryData[props.index].name + ' x 1 for $' + bakeryData[props.index].price}</p>
+        </>
+    )
 }
 
 export default App;
